ec5e76358d94cc09ab8fd1fa7f72ea9c7cbe3673
Oscar David Peñuela <odpenuelar@unal.edu.co>
2024-09-06 20:29:25 +0000

    se sube test mt32

--- TestU01/main.cpp
+++ TestU01/main.cpp
@@ -10,7 +10,7 @@ extern "C" {
 // Función generadora que devuelve números aleatorios normalizados a [0, 1]
 double generarAleatorioMt64() {
     static std::random_device rd;
-    static std::mt19937_64 generador(rd());
+    static std::mt19937 generador(rd());
     static std::uniform_real_distribution<double> distribucion(0.0, 1.0);
     return distribucion(generador);
 }
@@ -18,11 +18,11 @@ double generarAleatorioMt64() {
 int main() {
     // Crear un generador para TestU01
     // Se convierte la cadena constante a char* de forma explícita
-    char* nombre = const_cast<char*>("MT19937_64");
+    char* nombre = const_cast<char*>("MT19937_32");
     unif01_Gen* gen = unif01_CreateExternGen01(nombre, generarAleatorioMt64);
 
     // Ejecutar pruebas con TestU01
-    std::cout << "Ejecutando prueba SmallCrush MT64..." << std::endl;
+    std::cout << "Ejecutando prueba SmallCrush MT32..." << std::endl;
     bbattery_SmallCrush(gen);
 
     //std::cout << "Ejecutando prueba Crush..." << std::endl;
